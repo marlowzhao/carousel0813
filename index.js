3e029be48299f43c9ee2c38af46b5d2d9523d6c6
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-08-13 22:00:25 +0200

    able to view prev photos

--- index.js
+++ index.js
@@ -6,21 +6,30 @@ document.getElementById("carousel-button-prev").addEventListener("click", moveTo
 
 document.getElementById("carousel-button-next").addEventListener("click", moveToNextSlide);
 
-function moveToPrevSlide(){
-  console.log("prev")
-};
+function moveToPrevSlide() {
+  hideAllSlides();
+  if (slidePosition===0){
+    slidePosition = totalSlides-1;
+  }else{
+    slidePosition-=1;
+  }
+  slides[slidePosition].classList.add("carousel-item-visible");
+}
 
-// function moveToNextSlide(){
-//   console.log("next")
-// };
 
+function hideAllSlides() {
+  for (let slide of slides){
+    slide.classList.remove("carousel-item-visible");
+    slide.classList.add("carousel-item-hidden");
+  }
+}
 
-
-function moveToNextSlide(){
+function moveToNextSlide() {
+  hideAllSlides();
   if (slidePosition === totalSlides - 1){
     slidePosition = 0;
   } else {
-    slidePosition ++;
-  };
+    slidePosition++;
+  }
   slides[slidePosition].classList.add("carousel-item-visible");
 }
